6b42d41af002cc13567fbeff2e243cd63ecf7276
Krzysztof Kostecki <krzysztof.kosteckiagh@gmail.com>
2024-12-03 08:41:10 +0100

    Streamline day03 solutuion

--- src/day03.rs
+++ src/day03.rs
@@ -1,21 +1,7 @@
 use regex::Regex;
 
 
-fn parse_input_1(input: &str) -> Vec<(i32,i32)> {
-    let mut valid_muls = Vec::new();
-
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    for cap in re.captures_iter(input) {
-        println!("{:?}", cap);
-        let a = cap[1].parse::<i32>().unwrap();
-        let b = cap[2].parse::<i32>().unwrap();
-        valid_muls.push((a, b));
-    }
-
-    valid_muls
-}
-
-fn parse_input_2(input: &str) -> Vec<(i32,i32)> {
+fn parse_input(input: &str, enable_conditional_logic: bool) -> Vec<(i32,i32)> {
     let mut valid_muls = Vec::new();
 
     let re = Regex::new(r"(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))").unwrap();
@@ -26,7 +12,7 @@ fn parse_input_2(input: &str) -> Vec<(i32,i32)> {
                 "do()" => enable = true,
                 "don't()" => enable = false,
                 _ => {
-                    if enable {
+                    if !enable_conditional_logic || enable {
                         let a = cap[2].parse::<i32>().unwrap();
                         let b = cap[3].parse::<i32>().unwrap();
                         valid_muls.push((a, b));
@@ -40,7 +26,7 @@ fn parse_input_2(input: &str) -> Vec<(i32,i32)> {
 }
 
 pub fn part1(input: &str) -> i32 {
-    let valid_muls = parse_input_1(input);
+    let valid_muls = parse_input(input, false);
 
     let mut total = 0;
     for (a, b) in valid_muls {
@@ -51,7 +37,7 @@ pub fn part1(input: &str) -> i32 {
 }
 
 pub fn part2(input: &str) -> i32 {
-    let valid_muls = parse_input_2(input);
+    let valid_muls = parse_input(input, true);
 
     let mut total = 0;
     for (a, b) in valid_muls {
